5e0ac68af745ad1a49ae2ddbc8c0087968735cdf
unknown <subaru4ten@gmail.com>
2021-10-24 23:07:06 +0100

    updated

--- component/Teams.tsx
+++ component/Teams.tsx
@@ -7,21 +7,69 @@ const Teams = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-12 text-center">
-                        <p>Our Teams</p>
+                        <h4>Our Teams</h4>
                     </div>
                 </div>
                 <div style={{backgroundImage: "url(/images/teambg.png)",
                  backgroundRepeat: "no-repeat",  backgroundColor: "rgba(0, 0, 0, 1)",
                  backgroundSize: "100% 100%", width: "100%"}}>
-                        <div className="team-container d-flex gap-3 justify-content-center align-items-center position-relative">
-                            <div style={{backgroundImage: "url(/images/card_img.png)",backgroundRepeat: "no-repeat", backgroundSize: "100% 100%", width: "100%"}}>
+                        <div className="team-container d-flex gap-3 justify-content-center align-items-center">
+                            <div className="position-relative"  style={{backgroundImage: "url(/images/card_img.png)",backgroundRepeat: "no-repeat", backgroundSize: "100% 100%", width: "100%"}}>
                                 <Image src="/images/profile1.png" alt="profile" width={400} height={500} />
+                                <div className="team-social d-flex position-absolute gap-4">
+                                    <div className="px-4">
+                                        <div>
+                                            <a href="#"><Image src="/images/twitter-g.png" alt="profile" width={14} height={14} /></a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <Image src="/images/linkedin-g.png" alt="profile" width={14} height={14} />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div className="text-center">
+                                        <p id="team-green">Diego Lopez</p>
+                                        <p>CEO & Founder</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div style={{backgroundImage: "url(/images/card_img.png)",backgroundRepeat: "no-repeat", backgroundSize: "100% 100%", width: "100%"}}>
+                            <div className="position-relative" style={{backgroundImage: "url(/images/card_img.png)",backgroundRepeat: "no-repeat", backgroundSize: "100% 100%", width: "100%"}}>
                                 <Image src="/images/profile2.png" alt="profile" width={400} height={500} />
+                                <div className="team-social d-flex position-absolute gap-4">
+                                    <div className="px-4">
+                                        <div>
+                                            <a href="#"><Image src="/images/twitter-g.png" alt="profile" width={14} height={14} /></a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <Image src="/images/linkedin-g.png" alt="profile" width={14} height={14} />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div className="text-center">
+                                        <p id="team-green">Diego Lopez</p>
+                                        <p>CEO & Founder</p>
+                                    </div>
+                                </div>
                             </div>
-                            <div style={{backgroundImage: "url(/images/card_img.png)",backgroundRepeat: "no-repeat", backgroundSize: "100% 100%", width: "100%"}}>
+                            <div className="position-relative" style={{backgroundImage: "url(/images/card_img.png)",backgroundRepeat: "no-repeat", backgroundSize: "100% 100%", width: "100%"}}>
                                 <Image src="/images/profile3.png" alt="profile" width={400} height={500} />
+                                <div className="team-social d-flex position-absolute gap-4">
+                                    <div className="px-4">
+                                        <div>
+                                            <a href="#"><Image src="/images/twitter-g.png" alt="profile" width={14} height={14} /></a>
+                                        </div>
+                                        <div>
+                                            <a href="#">
+                                                <Image src="/images/linkedin-g.png" alt="profile" width={14} height={14} />
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div className="text-center">
+                                        <p id="team-green">Diego Lopez</p>
+                                        <p>CEO & Founder</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                 </div>
