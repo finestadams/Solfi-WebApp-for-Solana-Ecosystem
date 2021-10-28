48a7893992ed87693b2de6190e7add2c1ec2a8c0
unknown <subaru4ten@gmail.com>
2021-10-28 23:56:45 +0100

    udated

--- component/Explore.tsx
+++ component/Explore.tsx
@@ -1,18 +1,16 @@
-import React, { useEffect, useState, useRef, useCallback } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import {Fade} from 'react-awesome-reveal'
 
 
 const Explore = () => {
-        const handleWheel = () => {
-            console.log("wheel")
-        }
+      
       return (
         <div  className="explore_section explore-img position-relative" style={{backgroundImage: "url(/images/sky.png)",
         backgroundRepeat: "no-repeat",backgroundAttachment:"fixed", minHeight: "100vh",
-        backgroundSize: "cover", width: "100%"}}>
+        backgroundSize: "100% 100%", width: "100%"}}>
           <div  className="container">
-            <div onMouseEnter={handleWheel} className="row text-center justify-content-center">
+            <div  className="row text-center justify-content-center">
                 <div className="row ">
                     <div className="col-sm-10 offset-sm-1 ">
                         <Fade direction="up">
