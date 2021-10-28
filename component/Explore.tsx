ddb659f29fc936a431cb9954a07c0e82981a7ccf
unknown <subaru4ten@gmail.com>
2021-10-28 23:59:15 +0100

    updated

--- component/Explore.tsx
+++ component/Explore.tsx
@@ -8,7 +8,7 @@ const Explore = () => {
       return (
         <div  className="explore_section explore-img position-relative" style={{backgroundImage: "url(/images/sky.png)",
         backgroundRepeat: "no-repeat",backgroundAttachment:"fixed", minHeight: "100vh",
-        backgroundSize: "100% 100%", width: "100%"}}>
+        backgroundSize: "cover", width: "100%"}}>
           <div  className="container">
             <div  className="row text-center justify-content-center">
                 <div className="row ">
