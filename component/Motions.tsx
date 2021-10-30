1237b4ea9075adbe33bca16d300b781173d6a415
unknown <subaru4ten@gmail.com>
2021-10-30 14:29:31 +0100

    updated

--- component/Motions.tsx
+++ component/Motions.tsx
@@ -1,10 +1,17 @@
 import React, { useState } from 'react'
 import Image from 'next/image'
+import { FcAdvance } from "react-icons/fc";
+import { Fade } from 'react-awesome-reveal';
 
  const Motions = () => {
-     
+     const [isShow, setShow] = useState(1)
+
      const handleClick = () => {
+         setShow(isShow + 1)
         console.log("clicked");
+        if(isShow >= 4){
+            setShow(1)
+        }
      }
     return (
         <>
@@ -30,9 +37,10 @@ import Image from 'next/image'
                         <div className="orbit moon-orbit"></div>
                         <div className="moon-spin">
                         <div id="moon"></div>
+                        <span style={{color: "white"}}>Tier3</span>
                         </div>
                         <img id="earth" src="/images/purple_star.png" />
-                        <span style={{color: "white"}}>Tier3</span>
+                       
                     </div>
                     <div className="orbit mars-orbit"></div>
                     <div className="mars-spin">
@@ -43,23 +51,39 @@ import Image from 'next/image'
                     </div>           
                     </div>
                     <div style={{color: "white"}}>
-                       <div>
-                        <h1>Tier1 <span><Image src="/images/red_star.png" alt="" width={30} height={30} /></span></h1>
-                            <p>This is an example of Tier1</p>
-                            
-                       </div>
-                        <div>
-                            <h1>Tier2 <span><Image src="/images/blue_star.png" alt="" width={30} height={30} /></span></h1>
-                            <p>This is an example of Tier2</p>
-                        </div>
-                        <div>
+                       {
+                           isShow == 1 ? <div>
+                                <Fade direction="up">
+                           <h1>Tier1 <span><Image src="/images/red_star.png" alt="" width={30} height={30} /></span></h1>
+                               <p>This is an example of Tier1</p>
+                               </Fade>
+                               
+                          </div>: ''
+                       }
+                       {
+                           isShow == 2 ? 
+                            <div>
+                                 <Fade direction="up">
+                           <h1>Tier2 <span><Image src="/images/blue_star.png" alt="" width={30} height={30} /></span></h1>
+                           <p>This is an example of Tier2</p>
+                           </Fade>
+                       </div>: ''
+                       }
+                       {isShow == 3 ? <div>
+                        <Fade direction="up">
                             <h1>Tier3 <span><Image src="/images/purple_star.png" alt="" width={30} height={30} /></span></h1>
                             <p>This is an example of Tier3</p>
-                        </div>
-                        <div>
+                        </Fade>
+                        </div>: ''}
+                        {isShow == 4 ? <div>
+                            <Fade direction="up">
                             <h1>Tier4 <span><Image src="/images/yellow_star.png" alt="" width={30} height={30} /></span></h1>
                             <p>This is an example of Tier4</p>
-                        </div>
+                            </Fade>
+                        </div>: ''}
+                    </div>
+                    <div onClick={handleClick} className="animation-dir">
+                        <Image src="/images/right-arrow.svg" className="cursor-pointer" alt="arrow" width={50} height = {50}/>
                     </div>
             </div>    
         </>
